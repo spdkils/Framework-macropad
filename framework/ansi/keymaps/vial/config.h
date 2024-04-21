c2e7ccfab436d746b26013ad5b7c17f94b0c6b3c
spdkils <spdkils@hotmail.com>
2024-04-21 10:27:29 -0700

    Fix unlock matrix...
    Old unlock was impossible to key in, had to re-plug laptop keyboard while holding both alt.

--- framework/ansi/keymaps/vial/config.h
+++ framework/ansi/keymaps/vial/config.h
@@ -1,4 +1,4 @@
 #pragma once
 #define VIAL_KEYBOARD_UID {0x13, 0x4A, 0xA9, 0x01, 0x11, 0x1B, 0x3E, 0x18}
-#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
-#define VIAL_UNLOCK_COMBO_COLS {0, 2}
+#define VIAL_UNLOCK_COMBO_ROWS {1, 0}
+#define VIAL_UNLOCK_COMBO_COLS {3, 3}
